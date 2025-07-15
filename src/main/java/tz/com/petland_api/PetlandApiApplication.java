4bde0164bcd3e65b8e14ba904d3d4b4440a7635b
User <user@MacBook-Pro-de-User.local>
2025-07-15 20:59:05 +0100

    primeiro commit, configurando petshop

---
+++ src/main/java/tz/com/petland_api/PetlandApiApplication.java
@@ -0,0 +1,13 @@
+package tz.com.petland_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetlandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetlandApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tz/com/petland_api/PetlandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tz.com.petland_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetlandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
