846547c84daf0bae0fcb5450558f55e4a3f3799b
User <user@MacBook-Pro-de-User.local>
2025-07-24 22:30:59 +0100

    animalControllerFinadAll

---
+++ src/main/java/tz/com/petland_api/controllers/animalController/AnimalControllerFindAll.java
@@ -0,0 +1,29 @@
+package tz.com.petland_api.controllers.animalController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tz.com.petland_api.model.useCase.useCaseAnimal.UseCaseAnimalFindAll;
+
+@RestController
+@RequestMapping("/animais")
+public class AnimalControllerFindAll {
+
+    @Autowired
+    private UseCaseAnimalFindAll useCase;
+
+    @GetMapping("/")
+    public ResponseEntity<?> findAllAnimal(){
+        
+        try{
+            var dto = useCase.findAllAnimal();
+            return ResponseEntity.ok().body(dto);
+        }catch(Exception ex){
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
+    
+}
