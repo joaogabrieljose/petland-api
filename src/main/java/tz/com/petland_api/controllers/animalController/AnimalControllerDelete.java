3b5b15052613e79bed2c17f15b2a2798196d3bb5
User <user@MacBook-Pro-de-User.local>
2025-07-25 22:11:30 +0100

    AnimalControllerDelete

---
+++ src/main/java/tz/com/petland_api/controllers/animalController/AnimalControllerDelete.java
@@ -0,0 +1,31 @@
+package tz.com.petland_api.controllers.animalController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tz.com.petland_api.model.useCase.useCaseAnimal.UseCaseAnimalDelete;
+
+@RestController
+@RequestMapping("/animais")
+public class AnimalControllerDelete {
+
+    @Autowired
+    private UseCaseAnimalDelete useCase;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteController(@PathVariable Integer id){
+
+        try {
+            useCase.deleteAnimal(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+    
+}
