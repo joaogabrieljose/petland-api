bed8464aa45e6429244e7bd1dedf4d488149f75d
User <user@MacBook-Pro-de-User.local>
2025-07-25 22:00:44 +0100

    useCaseDeleteAnimal

---
+++ src/main/java/tz/com/petland_api/controllers/animalController/AnimalControllerUpdate.java
@@ -0,0 +1,33 @@
+package tz.com.petland_api.controllers.animalController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tz.com.petland_api.model.dto.animalDTO.AnimalDTORequest;
+import tz.com.petland_api.model.useCase.useCaseAnimal.useCaseAnimalUpdate;
+
+@RestController
+@RequestMapping("/animais")
+public class AnimalControllerUpdate {
+
+    @Autowired
+    private useCaseAnimalUpdate useCase;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable Integer id, @RequestBody AnimalDTORequest request){
+
+        try{
+            var dto = useCase.updateAnimal(id, request);
+            return ResponseEntity.ok().build();
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+    
+}
