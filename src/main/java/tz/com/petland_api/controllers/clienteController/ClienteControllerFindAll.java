0f7f177ce7fef258a75bc6e12e437f0a821df6f5
User <user@MacBook-Pro-de-User.local>
2025-07-26 00:39:18 +0100

    ClienteControllerFindAll

---
+++ src/main/java/tz/com/petland_api/controllers/clienteController/ClienteControllerFindAll.java
@@ -0,0 +1,29 @@
+package tz.com.petland_api.controllers.clienteController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tz.com.petland_api.model.useCase.useCaseCliente.UseCaseClienteFindAll;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteControllerFindAll {
+
+    @Autowired
+    private UseCaseClienteFindAll useCase;
+
+    @GetMapping("/")
+    public ResponseEntity<Object> findAllClientes(){
+
+        try {
+            var novoCliente = useCase.clienteFindAll();
+            return ResponseEntity.ok().body(novoCliente);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    
+}
